6430c32513677bec038d6af0758e74e8d163d8cc
Michael Oliyide <michaeloliyide86@gmai.com>
2024-11-19 15:19:19 +0100

    version 1.1

---
+++ script.js
@@ -0,0 +1,40 @@
+document.getElementById('menu-icon').addEventListener('click', function () {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show'); // Toggles navbar visibility
+  });
+  
+
+  // place order
+
+  (function() {
+    emailjs.init(''); 
+  })();
+
+  document.getElementById('orderForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+  
+    // Collect form data
+    const formData = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      phone: document.getElementById('phone').value,
+      order_type: document.getElementById('order-type').value,
+      delivery_date: document.getElementById('delivery-date').value,
+      details: document.getElementById('details').value,
+    };
+  
+    // Send form data via EmailJS
+    emailjs.send('service_ap0twre', 'template_he9wyin', formData, 'KaDpj-tt1XwtKgihx')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        document.getElementById('confirmationMessage').textContent = 'Order submitted successfully!';
+        document.getElementById('confirmationMessage').style.display = 'block';
+        document.getElementById('confirmationMessage').style.color = 'green';
+        document.getElementById('orderForm').reset();
+      }, (error) => {
+        console.error('FAILED...', error);
+        document.getElementById('confirmationMessage').textContent = 'Failed to submit order. Please try again.';
+        document.getElementById('confirmationMessage').style.display = 'block';
+        document.getElementById('confirmationMessage').style.color = 'red';
+      });
+  });
